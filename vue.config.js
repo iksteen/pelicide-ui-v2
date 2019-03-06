67f830279d8d02c32dfbf6aebb5db3e9cb1e24d2
Ingmar Steen <iksteen@gmail.com>
2019-03-06 22:25:31 +0100

    Compress production builds with gzip.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/app/' : '/',
   devServer: {
@@ -10,5 +12,16 @@ module.exports = {
         target: 'http://localhost:6300'
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(js|css|map|woff2?|eot|ttf)$'),
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
   }
 }
