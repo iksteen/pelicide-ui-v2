d416c7e908333d9f8b70256d63609fcf3b26f3c3
Ingmar Steen <iksteen@gmail.com>
2019-03-07 15:32:01 +0100

    U-Turn: Move formatting functions into editor.
    If they're in the toolbar, we'll have a much harder time to set up
    keymaps.

--- src/plugins/editors/markdown/markdown-editor.js
+++ src/plugins/editors/markdown/markdown-editor.js
@@ -14,6 +14,56 @@ export default {
         mode: 'markdown',
         indentUnit: 4
       }
+    },
+    bold () {
+      this.surroundSelection('**').focus()
+    },
+    italic () {
+      this.surroundSelection('_').focus()
+    },
+    strikethrough () {
+      this.surroundSelection('<s>', '</s>').focus()
+    },
+    header () {
+      this.replaceInLines(
+        /^(#+)\s?/,
+        match => match
+          ? [
+            match[1].length < 6 ? '#'.repeat(match[1].length + 1) + ' ' : '',
+            match.index,
+            match[0].length
+          ]
+          : ['# ']
+      ).focus()
+    },
+    link () {
+      const selection = this.getSelection()
+      if (!selection) {
+        this.surroundSelection('[](https://)', '', 11)
+      } else if (/^\w+:\/\/|^mailto:/.test(selection)) {
+        this.surroundSelection('[](', ')', selection.length + 3)
+      } else {
+        this.surroundSelection('[', '](https://)', 1)
+      }
+      this.focus()
+    },
+    image () {
+      const selection = this.getSelection()
+      if (!selection) {
+        this.surroundSelection('![]()', '', 3)
+      } else if (/^\w+:\/\//.test(selection)) {
+        this.surroundSelection('![](', ')', selection.length + 3)
+      } else {
+        this.surroundSelection('![', ']()', 1)
+      }
+      this.focus()
+    },
+    ulist () {
+      this.replaceInLines(
+        /^\s*[*+-]\s/,
+        match => match ? ['', 0, match[0].length] : ['* '],
+        !!this.getSelection()
+      ).focus()
     }
   }
 }
--- src/plugins/editors/markdown/markdown-toolbar.vue
+++ src/plugins/editors/markdown/markdown-toolbar.vue
@@ -2,32 +2,32 @@
   <v-toolbar-items>
     <panel-toolbar-button
       icon="mdi-format-bold"
-      @click="bold"
+      @click="editor.bold"
     />
     <panel-toolbar-button
       icon="mdi-format-italic"
-      @click="italic"
+      @click="editor.italic"
     />
     <panel-toolbar-button
       icon="mdi-format-strikethrough"
-      @click="strikethrough"
+      @click="editor.strikethrough"
     />
     <panel-toolbar-button
       icon="mdi-format-header-pound"
-      @click="header"
+      @click="editor.header"
     />
     <panel-toolbar-divider />
     <panel-toolbar-button
       icon="mdi-link-plus"
-      @click="link"
+      @click="editor.link"
     />
     <panel-toolbar-button
       icon="mdi-image"
-      @click="image"
+      @click="editor.image"
     />
     <panel-toolbar-button
       icon="mdi-format-list-bulleted"
-      @click="ulist"
+      @click="editor.ulist"
     />
   </v-toolbar-items>
 </template>
@@ -45,60 +45,6 @@
     data () {
       return {
         editor: this.getEditorComponent()
-      }
-    },
-    methods: {
-      bold () {
-        this.editor.surroundSelection('**').focus()
-      },
-      italic () {
-        this.editor.surroundSelection('*').focus()
-      },
-      strikethrough () {
-        this.editor.surroundSelection('<s>', '</s>').focus()
-      },
-      header () {
-        this.editor.replaceInLines(
-          /^(#+)\s?/,
-          match => match
-            ? [
-              match[1].length < 6 ? '#'.repeat(match[1].length + 1) + ' ' : '',
-              match.index,
-              match[0].length
-            ]
-            : ['# ']
-        ).focus()
-      },
-      link () {
-        const { editor } = this
-        const selection = editor.getSelection()
-        if (!selection) {
-          editor.surroundSelection('[](https://)', '', 11)
-        } else if (/^\w+:\/\/|^mailto:/.test(selection)) {
-          editor.surroundSelection('[](', ')', selection.length + 3)
-        } else {
-          editor.surroundSelection('[', '](https://)', 1)
-        }
-        editor.focus()
-      },
-      image () {
-        const { editor } = this
-        const selection = editor.getSelection()
-        if (!selection) {
-          editor.surroundSelection('![]()', '', 3)
-        } else if (/^\w+:\/\//.test(selection)) {
-          editor.surroundSelection('![](', ')', selection.length + 3)
-        } else {
-          editor.surroundSelection('![', ']()', 1)
-        }
-        editor.focus()
-      },
-      ulist () {
-        this.editor.replaceInLines(
-          /^\s*[*+-]\s/,
-          match => match ? ['', 0, match[0].length] : ['* '],
-          !!this.editor.getSelection()
-        ).focus()
       }
     }
   }
